03a9fb522412b75e77963177dae47106be08c66c
YooJinRa <rarastudy@naver.com>
2023-08-05 21:13:07 +0900

    feat: gh-pages 설치

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: "/YooJinRa.github.io",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
